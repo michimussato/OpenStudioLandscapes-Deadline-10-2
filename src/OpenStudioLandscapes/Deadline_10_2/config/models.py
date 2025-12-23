f58fcef72c7fefe21bc83b29cbd231439f2b67f8
Michael Mussato <michimussato@gmail.com>
2025-12-23 13:27:19 +0100

    47-refactor-to-config-based-landscapes: fully dynamic CONFIG_STR

--- src/OpenStudioLandscapes/Deadline_10_2/config/models.py
+++ src/OpenStudioLandscapes/Deadline_10_2/config/models.py
@@ -8,18 +8,16 @@ from pydantic import (
 
 LOGGER = get_dagster_logger(__name__)
 
+from OpenStudioLandscapes.engine.config.str_gen import get_config_str
 from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
 
 from OpenStudioLandscapes.Deadline_10_2 import dist
 
 config_default = pathlib.Path(__file__).parent.joinpath("config_default.yml")
-CONFIG_STR = config_default.read_text()
 
 
 class Config(FeatureBaseModel):
     feature_name: str = dist.name
-
-    definitions: str = "OpenStudioLandscapes.Deadline_10_2.definitions"
 
     enabled: bool = False
 
@@ -293,3 +291,9 @@ class Config(FeatureBaseModel):
             )
         )
         return ret
+
+
+CONFIG_STR = get_config_str(
+    Config=Config,
+)
+
